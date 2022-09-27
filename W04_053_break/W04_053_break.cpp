52caafdb6bef1c15b9caf9bb9aa6ce32d92ad128
solove0416 <114461904+solove0416@users.noreply.github.com>
2022-09-27 13:56:33 +0900

    Add files via upload

---
+++ W04_053_break/W04_053_break.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n;
+	
+	printf("n을 입력: ");
+	scanf_s("%d",  &n);
+	
+	//for (int i = 1; i <= n; i++)
+	//	if (i % 3 == 0)
+	//		continue;//3의 배수라면 출력하지 않고 다시 위로 올라간다.
+	//	else
+	//		printf("%d\n", i);
+	for (int i = 1; i <= n; i++) {
+		if (i % 3 == 0)
+			break;
+		printf("%d\n", i);
+	}
+
+
+}
---
+++ W05_056_gugudan/W05_056_gugudan.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+//구구단(5단)
+/*
+	5 x 1 = 5
+	5 x 2 = 10
+	  ..
+	5 x 9 = 45
+	두 번째 숫자가 1~9까지 변화(반복문 1개 필요)
+	*/
+int main()
+{
+	int n;
+
+	printf("n을 입력:");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= 9; i++)
+		printf("%d x %d = %d\n", n, i, n*i);
+
+}
